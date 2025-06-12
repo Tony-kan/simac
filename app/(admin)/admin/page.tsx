5006411f2a42e99cf2a2be0169fa12d189d07a43
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-06-12 23:07:25 +0200

    build fix 2

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Page = () => {
+const AdminPage = () => {
   return <div>Page</div>;
 };
 
-export default Page;
+export default AdminPage;
--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Page = () => {
+const UserPage = () => {
   return <div>Page</div>;
 };
 
-export default Page;
+export default UserPage;
