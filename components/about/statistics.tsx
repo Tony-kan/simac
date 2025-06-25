4b7138526541885958bf71fb275cae980d9928ee
Samuel Gondwe <samuelgondwe741@gmail.com>
2025-06-25 23:24:15 +0200

    About: add the docs

--- components/about/statistics.tsx
+++ components/about/statistics.tsx
@@ -2,18 +2,30 @@ import React from "react";
 
 /**
  * StatisticItem represents a single statistic with a label and value.
+ * @property label - The label or description for the statistic (string)
+ * @property value - The value of the statistic (number or string, e.g., '+ 20000')
  */
 interface StatisticItem {
   label: string;
   value: number | string;
 }
 
+/**
+ * Array of statistics to be displayed in the Statistics component.
+ * Each item contains a label and a value.
+ */
 const statisticsData: StatisticItem[] = [
   { label: "Graduates", value: "+ 20000" },
   { label: "Annual Performances", value: "+ 30 " },
   { label: "Empowered Children", value: "+ 1000" },
 ];
 
+/**
+ * Statistics Component
+ *
+ * Renders a responsive set of statistic cards, each showing a label and value.
+ * The data is defined internally and not passed as props.
+ */
 const Statistics: React.FC = () => {
   return (
     <div className="flex flex-wrap gap-6 bg-[#FFE3BB] justify-between">
