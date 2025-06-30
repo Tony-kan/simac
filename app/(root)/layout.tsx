ced0419761b348758c9a1be1d221554f6431642b
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-06-30 16:14:44 +0200

    modify h

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="h-screen">
+    <main className="h-full min-h-screen">
       <MainHeader />
       {children}
     </main>
